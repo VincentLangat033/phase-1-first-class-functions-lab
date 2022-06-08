f08e77387edbe2c909a54fd13af1eb5202d8c517
Vincent Langat <kimutailangat033@gmail.com>
2022-06-08 17:08:14 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+const returnFirstTwoDrivers = (drivers)=>([drivers[0],drivers[1]])
+// alternatively 
+// const returnFirstTwoDrivers = (drivers)=>{
+//     return [drivers[0],drivers[1]]
+// }
+//Function to return The last two drivers
+const returnLastTwoDrivers= (drivers)=>([drivers[2],drivers[3]])
+//This function has returnFirstTwo && LastTwoDrivers as its elements
+
+const selectingDrivers=[returnFirstTwoDrivers,returnLastTwoDrivers];
+
+//create a function that returns a function
+function createFareMultiplier(myDestin){
+    return  function(amountPaid){
+        return myDestin*amountPaid;
+    }
+
+}
+//A function fareDoubler that returns doublefare
+// function fareDoubler(myfare){
+//  return myfare*2;
+// }
+// //A function fareTrippler that tripples fare
+// function fareTripler(myfare){
+//     return myfare*3;
+// }
+//The two above functions work and return  DoubleFare and TrippleFare but we can use createFaremultiplier as indicated in Test.js
+
+const fareDoubler = createFareMultiplier(2);
+//The above implements first class function and passes an argument directly to CreatFareMultiplier
+const fareTripler = createFareMultiplier(3);
+ //use arrow functions
+ const selectDifferentDrivers =(arrayOfDrivers,anyTwoDriver)=>anyTwoDriver(arrayOfDrivers);
+
